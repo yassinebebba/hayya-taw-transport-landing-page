91af2d4972389962fddf48704eb042157c9798c7
mohamedbebba <mohamedbebba1@gmail.com>
2024-06-30 19:18:10 +0200

    news

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import Camp from "@/components/Camp";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <Hero />
