b05cbed0866ca5259cab4e199060f8d523c9a219
mohamedbebba <mohamedbebba1@gmail.com>
2024-06-30 22:10:46 +0200

    news

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -47,7 +47,6 @@ const Hero = () => {
           <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
-              <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
             <p className="bold-20 text-white">Gafsa, Tunisia</p>
           </div>
