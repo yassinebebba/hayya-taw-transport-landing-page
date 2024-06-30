2f23ff3ec5e61bec265abfc1d493cc213241dd39
mohamedbebba <mohamedbebba1@gmail.com>
2024-06-30 23:49:13 +0200

    image pos

--- components/Features.tsx
+++ components/Features.tsx
@@ -9,7 +9,7 @@ const Features = () => {
       className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24"
     >
       <div className="max-container padding-container relative w-full flex justify-end">
-        <div className="flex flex-1 lg:min-h-[900px] ">
+        <div className="flex flex-1 lg:min-h-[900px] lg:-translate-x-10">
           <Image
             src="/phone.jpeg"
             alt="phone"
