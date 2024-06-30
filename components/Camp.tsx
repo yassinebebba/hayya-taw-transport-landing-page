da05607231109bffe925b47b621536d65fc81e2c
mohamedbebba <mohamedbebba1@gmail.com>
2024-06-30 19:06:42 +0200

    news

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -16,7 +16,8 @@ const CampSite = ({
 }: CampProps) => {
   return (
     <div
-      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+      className={`h-full w-full min-w-[1100px] bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
@@ -30,19 +31,8 @@ const CampSite = ({
         </div>
 
         <div className="flexCenter gap-6">
-          <span className="flex -space-x-4 overflow-hidden">
-            {PEOPLE_URL.map((url) => (
-              <Image
-                className="inline-block h-10 w-10 rounded-full"
-                src={url}
-                key={url}
-                alt="person"
-                width={52}
-                height={52}
-              />
-            ))}
-          </span>
-          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+          <span className="flex -space-x-4 overflow-hidden"></span>
+          {/* <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p> */}
         </div>
       </div>
     </div>
@@ -54,13 +44,13 @@ const Camp = () => {
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
-          backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
+          backgroundImage="/slider2.jpg"
+          title="Explore your city"
+          subtitle="Gafsa, Tunisa"
           peopleJoined="50+ Joined"
         />
         <CampSite
-          backgroundImage="bg-bg-img-2"
+          backgroundImage="/slider1.jpeg"
           title="Mountain View Camp"
           subtitle="Somewhere in the Wilderness"
           peopleJoined="50+ Joined"
